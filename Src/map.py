90a3c7088b74bb4331813b709daa81dbf5e6153d
Zichong <zichli@student.ethz.ch>
2022-12-05 11:36:15 +0100

    bug fixed

--- Src/map.py
+++ Src/map.py
@@ -2,14 +2,15 @@ from modelunit import ModelUnit
 import cv2 
 import numpy as np
 import math
-from utils import glob2img
+from utils import glob2img, timeit
 from station import SBBGraph
+from copy import deepcopy
 class Map:
     def __init__(self, population_map, sbb_graph, border):
         self.sbb_graph = sbb_graph
         self.border = border
         self.original_resolution = [] #format x y
-        self.resolution = [80, 40] #format x y
+        self.resolution = [320, 160] #format x y
         self.total_population = 0
         self.resample(population_map)
         self.init_model()
@@ -19,6 +20,7 @@ class Map:
     def resample(self, population , normalizing = True):
         self.original_resolution = population.shape[::-1]
         self.map_array = cv2.resize(population, self.resolution)
+        #self.map_array = cv2.GaussianBlur(self.map_array,(1,1),0)
         self.map_array *= math.prod(self.original_resolution)/math.prod(self.resolution)
         if normalizing:
             self.map_array /= np.nanmax(self.map_array)
@@ -28,7 +30,7 @@ class Map:
         self.model_array = [[ModelUnit(self.map_array[y,x], [y,x], size=size) for x in range(self.resolution[0])] for y in range(self.resolution[1])]
         for rows in self.model_array:
             for unit in rows:
-                if unit:
+                if not unit.empty:
                     unit.parent_model_array = self.model_array
                     unit.sbb_graph = self.sbb_graph
     
@@ -40,7 +42,7 @@ class Map:
                 if isinstance(self.model_array[img_pos[1]][img_pos[0]], ModelUnit):   
                     self.model_array[img_pos[1]][img_pos[0]].stations.append(station)
                     #self.model_array[img_pos[1]][img_pos[0]].population = 0.1
-                else:
+                elif self.model_array[img_pos[1]][img_pos[0]].on_border:
                     station_to_delete.append(key)
             else:   
                 station_to_delete.append(key)
@@ -49,10 +51,13 @@ class Map:
             for neighbour in self.sbb_graph.sbb_graph[key].neighbour.keys():
                 del self.sbb_graph.sbb_graph[neighbour].neighbour[key]
             del self.sbb_graph.sbb_graph[key]
+        for rows in self.model_array:
+            for unit in rows:
+                unit.init_neighbour_station() 
 
     def get_map_array(self, visualize_type="p"):
         if visualize_type=="p":
-            return self.plot_preparation(np.array(self.map_array))
+            return self.plot_preparation(np.array(self.map_array), base_map=True)
         elif visualize_type=="i":
             return self.plot_preparation(np.array([[ unit.i for unit in row] for row in self.model_array]))
         elif visualize_type=="s":
@@ -63,20 +68,31 @@ class Map:
             return self.plot_preparation(np.array([[unit.v for unit in row] for row in self.model_array]))
         else:
             return self.plot_preparation(np.array([[unit.d for unit in row] for row in self.model_array]))
-    def plot_preparation(self, default_map):
+            
+    def plot_preparation(self, default_map, base_map = False):
         offset = 1
-        default_map = np.nan_to_num(default_map, nan = 0.8 - offset )
+        scaling = 25
+        #default_map = np.nan_to_num(default_map, nan = 0.5 - offset )
+        if base_map:
+            default_map[default_map == 0] = (0.5 - offset)/scaling
         #default_map[default_map<1e-5]=1e-5
-        modified_map = np.log((default_map + offset))
+        modified_map = np.log((scaling*default_map + offset))/np.log(scaling + offset)
+        modified_map[0,0] = -1
+        modified_map[-1,-1] = 1
+        print("max: ",np.min(modified_map), "max: ",np.max(modified_map))
         return modified_map
 
+    @timeit
     def update_map(self):
         for rows in self.model_array:
             for unit in rows:
-                if unit:
+                if not unit.empty:
                     unit.evolution_step()
+        for rows in self.model_array:
+            for unit in rows:
+                if not unit.empty:
+                    unit.update()
         
-
 
 if __name__ == "__main__":
     CH_map = Map()
--- Src/modelunit.py
+++ Src/modelunit.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from random import random
 
 class ModelUnit:
@@ -11,9 +12,11 @@ class ModelUnit:
 
         self.cutoff_radius = 5 #km
         self.cutoff_delta = [int(self.cutoff_radius//size[0]), int(self.cutoff_radius//size[1])]
-        self.population = local_population_density if local_population_density > 0 else 0
+        self.population = local_population_density if local_population_density > 1e-5 else 0
+        self.empty = False if local_population_density > 1e-5 else True
+        
         self.sir_params = sir_params
-        self.delta = [0.7, 0.7, 0.0]
+        self.delta = [0.01, 0.01, 0.01]
         self.gamma = 0.001
         self.alpha = 0.1
         self.beta = 0.1
@@ -25,24 +28,31 @@ class ModelUnit:
         self.v = 0
         self.d = 0
 
-        self.neightbour = []
+        self.neightbour_stations = []
         self.stations = []
         self.on_border = False
+        self.neighbour_rel_pos = [0,0]
 
         self.inflow = 0
         self.outflow = 0
-   
-    def compute_inflow_sbb(self):
-        sbb_inflow = 0
+
+    def init_neighbour_station(self):
         if self.stations:
             for station in self.stations:
                 for neighbourID in station.neighbour.keys():
-                    neighbour_pos = self.sbb_graph.get_img_pos(neighbourID)
-                    if neighbour_pos[1] >= len(self.parent_model_array) or neighbour_pos[0] >= len(self.parent_model_array[0]):
-                        continue
-                    neighbour_unit = self.parent_model_array[neighbour_pos[1]][neighbour_pos[0]]
-                    if neighbour_unit:
-                        sbb_inflow += neighbour_unit.compute_outflow()
+                    if neighbourID not in self.neightbour_stations and neighbourID not in [station.name["ID"] for station in self.stations]:
+                        self.neightbour_stations.append(neighbourID)
+    
+    def compute_inflow_sbb(self):
+        sbb_inflow = 0
+        if self.stations:
+            for neighbourID in self.neightbour_stations:
+                neighbour_pos = self.sbb_graph.get_img_pos(neighbourID)
+                if neighbour_pos[1] >= len(self.parent_model_array) or neighbour_pos[0] >= len(self.parent_model_array[0]):
+                    continue
+                neighbour_unit = self.parent_model_array[neighbour_pos[1]][neighbour_pos[0]]
+                if neighbour_unit:
+                    sbb_inflow += neighbour_unit.compute_outflow()
             return sbb_inflow
         else:
             return 0
@@ -50,49 +60,64 @@ class ModelUnit:
 
     def compute_inflow_local(self):
         local_inflow = 0 
-        for delta_x in range(-self.cutoff_delta[0],self.cutoff_delta[0]):
-            if self.pos[1] + delta_x < len(self.parent_model_array[0]):
-                for delta_y in range(-self.cutoff_delta[1],self.cutoff_delta[1]):
-                    if self.pos[0] + delta_y < len(self.parent_model_array):
+        for delta_x in range(-self.cutoff_delta[0],self.cutoff_delta[0]+1):
+            if self.pos[1] + delta_x < len(self.parent_model_array[0]) and self.pos[1] + delta_x > 0:
+                for delta_y in range(-self.cutoff_delta[1],self.cutoff_delta[1]+1):
+                    if self.pos[0] + delta_y < len(self.parent_model_array) and self.pos[0] + delta_y > 0:
                         if math.sqrt(delta_x**2 + delta_y**2) <= 2*self.cutoff_radius/(self.size[0]+self.size[1]):
-                            neighbour = self.parent_model_array[self.pos[0] + delta_y][self.pos[1] + delta_x ] 
-                            local_inflow += neighbour.outflow / (math.sqrt(delta_x**2 + delta_y**2)+ 1)
+                            if delta_x != 0 and delta_y!=0:
+                                if not self.parent_model_array[self.pos[0] + delta_y][self.pos[1] + delta_x ].empty:
+                                    neighbour = self.parent_model_array[self.pos[0] + delta_y][self.pos[1] + delta_x ] 
+                                    local_inflow += neighbour.outflow / ( math.sqrt(delta_x**2 + delta_y**2))
         return local_inflow
 
     def compute_inflow(self):
         #extract the number of infected people coming from the outside
-        self.inflow = self.compute_inflow_sbb() + self.compute_inflow_local()
-        pass
-        return self.inflow
+        self.inflow = self.compute_inflow_sbb() + self.compute_inflow_local() # +
+        return self.inflow * self.population
+        #return 0
 
     def compute_outflow(self):
         #compute the number of people who are leaving the current region
-        self.outflow = self.population * self.i
+        self.outflow = self.i * 0.1
         return self.outflow
 
     def evolution_step(self):
         self.compute_inflow()
         self.compute_outflow()
+
+    def update(self):
         self.update_SIR()
 
+    def isBorder(self):
+        has_neighbour = False
+        for delta_pos in [[0,1],[0,-1],[1,0],[-1,0],[-1,1],[-1,-1],[1,1],[1,-1]]: 
+            if not self.parent_model_array[self.pos[0],self.pos[1]+1].empty:
+                has_neighbour = True
+                self.neighbour_rel_pos = delta_pos
+                break
+        self.on_border = self.empty and has_neighbour
+
     def update_SIR(self):
-        input = self.inflow
+        input = min(self.s, self.inflow)
         s = self.s
         i = self.i
         r = self.r
         v = self.v
         p = self.population
+        if self.pos[0] == 34 and self.pos[1] == 182:
+            print("hi")
         if p>0:
-            self.s += self.alpha * i * s / p - self.delta[0]*s
-            self.i += self.alpha * i * s / p - (self.beta + self.gamma) * i - self.delta[1] * i + input
-            self.r += self.beta * i - self.delta[2] * r
-            self.v += self.delta[0] * s + self.delta[1] * i + self.delta[2] * r
+            self.s = s - self.alpha * i * s / p - self.delta[0]*s - input
+            self.i = i + self.alpha * i * s / p - (self.beta + self.gamma) * i - self.delta[1] * i + input
+            self.r = r + self.beta * i - self.delta[2] * r
+            self.v = v + self.delta[0] * s + self.delta[1] * i + self.delta[2] * r
             self.s = min(max(0, self.s), self.population)
             self.i = min(max(0, self.i), self.population)
             self.r = min(max(0, self.r), self.population)
             self.v = min(max(0, self.v), self.population)
-
             self.population = self.s + self.i + self.r + self.v
+
             self.d += self.gamma * i
 
 
